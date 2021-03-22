e40ba7442066d025e34e2e8093041dc3ec169baa
Zues Fugitive <stephaineanspach@gmail.com>
2021-03-22 18:03:47 +0100

    make it better

--- cards/mycards/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MycardsConfig(AppConfig):
-    name = 'mycards'
--- cards/mycards/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('card/',  views.button),
-    path('cards/', views.js_random, name= "script"),
-    path('xor/', views.xorshift, name= "scripts"),
-]
