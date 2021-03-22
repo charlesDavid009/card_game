a7a9d474fe9bfe3706b94838cdd9387942a81958
Zues Fugitive <stephaineanspach@gmail.com>
2021-03-22 13:55:21 +0100

    Third commit

--- cards/mycards/urls.py
+++ cards/mycards/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('cards/', views.RandomView.as_view()),
+    path('card/',  views.button),
+    path('cards/', views.js_random, name= "script"),
+    path('xor/', views.xorshift, name= "scripts"),
 ]
